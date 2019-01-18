8474a2386721e6edfb29d2aec2ffd27caeb4c3c6
Andrew McCarthy <amccarthy13@cs.uchicago.edu>
2019-01-17 19:26:45 -0600

    feat(model): create buisness model for db

--- mysite/map/models.py
+++ mysite/map/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+class Buisness(models.Model):
+    buisness_name = models.CharField(max_length=200)
+    GPS_X = models.FloatField(default=0)
+    GPS_Y = models.FloatField(default=0)
+    address = models.CharField(max_length=200)
+    student_discount = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.buisness_name
